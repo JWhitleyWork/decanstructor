d1d90737f55ac840e9839067bbb24dd3b454148a
Joshua Whitley <jwhitley@autonomoustuff.com>
2017-12-25 12:41:21 -0600

    Adding basic window.

--- include/decanstructor/decanstructor_node.h
+++ include/decanstructor/decanstructor_node.h
@@ -1,6 +1,49 @@
 #ifndef DECANSTRUCTOR_NODE_H
 #define DECANSTRUCTOR_NODE_H
 
+#include <wx/wxprec.h>
+#ifndef WX_PRECOMP
+    #include <wx/wx.h>
+#endif
 #include <ros/ros.h>
+
+namespace DeCANstructor
+{
+  enum
+  {
+    ID_Hello = 1
+  };
+
+  class DCNode :
+    public wxApp
+  {
+    public:
+      virtual bool OnInit();
+  };
+
+  class DCFrame :
+    public wxFrame
+  {
+    public:
+      DCFrame(const wxString& title,
+              const wxPoint& pos,
+              const wxSize& size);
+
+    private:
+      void OnHello(wxCommandEvent& event);
+      void OnExit(wxCommandEvent& event);
+      void OnAbout(wxCommandEvent& event);
+
+      wxDECLARE_EVENT_TABLE();
+  };
+
+	wxBEGIN_EVENT_TABLE(DCFrame, wxFrame)
+		EVT_MENU(ID_Hello,   DCFrame::OnHello)
+		EVT_MENU(wxID_EXIT,  DCFrame::OnExit)
+		EVT_MENU(wxID_ABOUT, DCFrame::OnAbout)
+	wxEND_EVENT_TABLE()
+}
+
+wxIMPLEMENT_APP(DeCANstructor::DCNode);
 
 #endif
--- src/decanstructor_node.cpp
+++ src/decanstructor_node.cpp
@@ -1,7 +1,48 @@
 #include <decanstructor_node.h>
 
-int main(int argc, char** argv)
-{
+using namespace DeCANstructor;
 
-  return 0;
+bool DCNode::OnInit()
+{
+  DCFrame* frame = new DCFrame("DeCANstructor", wxPoint(50, 50), wxSize(450, 340));
+  frame->Show(true);
+  return true;
+}
+
+DCFrame::DCFrame(const wxString& title,
+                 const wxPoint& pos,
+                 const wxSize& size) :
+  wxFrame(NULL, wxID_ANY, title, pos, size)
+{
+  wxMenu* menu_file = new wxMenu;
+  menu_file->Append(ID_Hello, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item");
+  menu_file->AppendSeparator();
+  menu_file->Append(wxID_EXIT);
+
+  wxMenu* menu_help = new wxMenu;
+  menu_help->Append(wxID_ABOUT);
+
+  wxMenuBar* menu_bar = new wxMenuBar;
+  menu_bar->Append(menu_file, "&File");
+  menu_bar->Append(menu_help, "&Help");
+
+  SetMenuBar(menu_bar);
+
+  CreateStatusBar();
+  SetStatusText("Welcome to DeCANstructor.");
+}
+
+void DCFrame::OnExit(wxCommandEvent& event)
+{
+  Close(true);
+}
+
+void DCFrame::OnAbout(wxCommandEvent& event)
+{
+	wxMessageBox("Copyright 2017 Joshua Whitley, All Rights Reserved", "About DeCANstructor", wxOK | wxICON_INFORMATION );
+}
+
+void DCFrame::OnHello(wxCommandEvent& event)
+{
+  wxLogMessage("Hey.");
 }
