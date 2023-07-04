51cc8ec74625dad96a259a9f2124bf240fbaf108
Joshua Whitley <josh@electrifiedautonomy.com>
2023-07-04 14:38:17 -0500

    Fixing up licenses.

--- include/decanstructor/message_analyzer.hpp
+++ include/decanstructor/message_analyzer.hpp
@@ -1,3 +1,9 @@
+// Copyright 2017-2023 Joshua Whitley
+// 
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file or at
+// https://opensource.org/licenses/MIT.
+
 #ifndef DECANSTRUCTOR__MESSAGE_ANALYZER_HPP_
 #define DECANSTRUCTOR__MESSAGE_ANALYZER_HPP_
 
